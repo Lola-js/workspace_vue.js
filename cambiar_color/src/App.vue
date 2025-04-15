b81db668a9851653985d965cb245161bbfa9b797
Dori Fdez <md.fernandez.sanchez@gmail.com>
2025-04-15 12:20:53 +0200

    primer commit

---
+++ cambiar_color/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Lista de cartas</h1>
+  <PostalContenedor />
+
+  <ContadorOption />
+
+  <ColorPostal />
+</template>
+
+<script setup>
+  import PostalContenedor from './components/PostalContenedor.vue'
+  import ContadorOption from './components/ContadorOption.vue'
+  import ColorPostal from './components/ColorPostal.vue'
+</script>
+
+<style>
+h1{
+  
+  color:blue;
+}
+</style>
---
+++ cambiar_color/src/components/PostalContenedor.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="post-container">
+        <PostalCarta />
+        <PostalCarta />
+        <PostalCarta />
+        <PostalCarta />
+    </div>
+    
+    </template>
+    
+    <script setup>
+        import PostalCarta from './PostalCarta.vue'
+    </script>
+    
+    <style>
+        .post-container{
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+        }
+    
+    </style>    
